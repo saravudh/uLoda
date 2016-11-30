0b08a0f6201f2be9137b6df959453ab03c042a70
Saravudh Sinsomros <saravudh@shorsher.com>
2016-11-30 13:01:20 +0700

    add podfile and SwiftyJSON to project

---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '9.0'
+use_frameworks!
+
+target 'uLoda' do
+  pod 'SwiftyJSON', '~> 3.1.3'
+end
---
+++ uLoda/LDDictionary.swift
@@ -0,0 +1,15 @@
+//
+//  LDictionary.swift
+//  uLoda
+//
+//  Created by Saravudh Sinsomros on 11/30/2559 BE.
+//  Copyright © 2559 Saravudh Sinsomros. All rights reserved.
+//
+
+import UIKit
+
+class LDDictionary {
+    func search() -> String {
+        return ""
+    }
+}
