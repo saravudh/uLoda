ed3164ecac53393d0eea466c086965519ccff617
Saravudh Sinsomros <saravudh@shorsher.com>
2016-11-30 12:34:28 +0700

    init project

---
+++ uLoda/uLoda.h
@@ -0,0 +1,19 @@
+//
+//  uLoda.h
+//  uLoda
+//
+//  Created by Saravudh Sinsomros on 11/30/2559 BE.
+//  Copyright © 2559 Saravudh Sinsomros. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for uLoda.
+FOUNDATION_EXPORT double uLodaVersionNumber;
+
+//! Project version string for uLoda.
+FOUNDATION_EXPORT const unsigned char uLodaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <uLoda/PublicHeader.h>
+
+
